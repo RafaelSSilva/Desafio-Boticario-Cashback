a5d15c0bb9a0f369fffb9086a874ba4e0d0d20b4
rafael_ceuapp <rafael@ceuapp.com>
2019-12-02 06:13:32 -0300

    correction:external-api

--- src/controllers/external-api.controller.js
+++ src/controllers/external-api.controller.js
@@ -15,41 +15,15 @@ module.exports = {
 
             axios.get(url, { headers: { token: 'ZXPURQOARHiMc6Y0flhRC1LVlZQVFRnm' } })
                 .then((response) => {
-                    const total = response.data.body.credit;
-                    const cashback = calculateCredit(total);
+                    const credit = response.data.body.credit;
 
                     return res.json({
-                        credit: cashback.value,
-                        percentage: cashback.percentage
+                        credit: credit,
                     });
                 });
 
         } catch (err) {
             return res.status(400).send({ error: err });
         }
-    }
-}
-
-
-
-/**
- * calculates the cridit of the purchase.
- * @param {number} total 
- * @returns Object
- */
-function calculateCredit(total) {
-    try {
-        if (total <= 999) {
-            let p = 10;
-            return { percentage: `${p}%`, value: total * (p / 100) };
-        } else if (total >= 1000 && total <= 1500) {
-            let p = 15;
-            return { percentage: `${p}%`, value: total * (p / 100) };
-        } else {
-            let p = 20;
-            return { percentage: `${p}%`, value: total * (p / 100) };
-        }
-    } catch (e) {
-        return { percentage: '0%', value: 0 };
     }
 }
